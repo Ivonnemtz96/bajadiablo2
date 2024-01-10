9f2390d6ec91b8adf315ac2f77e7ce2ab0f3b0f8
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-10 11:31:48 -0700

    nuevo diseño ok

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Inicio';
+$inicio = 'active';
+include('includes/head.php');
+?>
+
+<body>
+    <?
+        include('includes/header.php');
+        include('modules/index.php');
+        include('includes/footer.php');
+        include('includes/preloader.php');
+        include('includes/redes.php');
+        include('includes/scripts.php');
+    ?>
+</body>
+
+</html>
